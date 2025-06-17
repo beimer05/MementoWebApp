a54d319eb2eeb38a11c468100cb2755659b5d1ee
beimer05 <barroyor2022@gmail.com>
2025-06-17 16:57:16 -0500

    Create article editor

---
+++ Memento/ArticleEditor.cs
@@ -0,0 +1,26 @@
+﻿using MementoWebApp.Models;
+
+namespace MementoWebApp.Memento
+{
+    public class ArticleEditor
+    {
+        public Article Article { get; private set; }
+
+        public ArticleEditor(Article article)
+        {
+            Article = article;
+        }
+
+        public ArticleMemento CreateMemento()
+        {
+            return new ArticleMemento(Article.Title, Article.Body);
+        }
+
+        public void Restore(ArticleMemento memento)
+        {
+            Article.Title = memento.Title;
+            Article.Body = memento.Body;
+        }
+
+    }
+}
