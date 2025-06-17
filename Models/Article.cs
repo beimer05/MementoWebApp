9d79e5f59c4c0b92a2b3b0276f365ad2546e5eee
beimer05 <barroyor2022@gmail.com>
2025-06-17 16:52:40 -0500

    Create article model

---
+++ Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace MementoWebApp.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
