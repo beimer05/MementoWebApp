4b769425e936c260f630ca3cd88550f7e6ec4270
beimer05 <barroyor2022@gmail.com>
2025-06-17 16:55:48 -0500

    Create article memento

---
+++ Memento/ArticleMemento.cs
@@ -0,0 +1,13 @@
+﻿namespace MementoWebApp.Memento
+{
+    public class ArticleMemento
+    {
+        public string Title { get; }
+        public string Body { get; }
+
+        public ArticleMemento (string title, string body)
+        {
+            Title = title; Body = body;
+        }
+    }
+}
