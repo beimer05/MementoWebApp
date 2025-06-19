8199a9c05a09b7ba3eda0ffe725dbb4e0c216273
beimer05 <barroyor2022@gmail.com>
2025-06-19 15:28:09 -0500

    Correct code on method T Get<T>

--- Extensions/SessionExtensions.cs
+++ Extensions/SessionExtensions.cs
@@ -12,7 +12,7 @@ namespace MementoWebApp.Extensions
         public static T Get<T>(this ISession session, string key)
         {
             var value = session.GetString(key);
-            return value == null ? default : JsonSerializer.Deserialize<T>(value);
+            return value == null ? default  : JsonSerializer.Deserialize<T>(value);
         }
     }
 }
